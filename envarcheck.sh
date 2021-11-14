8713c06710610c3d3277e83aca62b4fe14ff0b19
iamabhi365 <iamabhi365@gmail.com>
2021-11-14 18:06:48 +0530

    Update envarcheck.sh

--- envarcheck.sh
+++ envarcheck.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-if [ -z "${!TASK_NAME}" ]; then
+if [[ -z "${TASK_NAME}" ]]; then
     echo "Environment variable TASK_NAME is not set" 2>&1 >> /var/log/envarcheck.log
 else
     echo "Environment variable TASK_NAME is set."
